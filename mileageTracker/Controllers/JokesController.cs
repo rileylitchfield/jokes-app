bf0d9cd5fe62bdde245e543a9cd1c4efbc94cc97
Riley <rileylitchfield95@gmail.com>
2022-11-19 12:33:40 -0600

    Added Search funcitonality

--- mileageTracker/Controllers/JokesController.cs
+++ mileageTracker/Controllers/JokesController.cs
@@ -25,6 +25,18 @@ namespace mileageTracker.Controllers
               return View(await _context.Joke.ToListAsync());
         }
 
+        // GET: Jokes/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();
+        }
+
+        // GET: Jokes/ShowSearchResults
+        public async Task<IActionResult> ShowSearchResults(String SearchPhrase)
+        {
+            return View("Index",await _context.Joke.Where(j => j.JokeQuestion.Contains(SearchPhrase) || j.JokeAnswer.Contains(SearchPhrase)).ToListAsync());
+        }
+
         // GET: Jokes/Details/5
         public async Task<IActionResult> Details(int? id)
         {
